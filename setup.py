7a1b46cea6b1ed6b87d74b7ab49c43773d0a2642
sasiegel <ssiegel21331@gmail.com>
2021-06-09 15:37:40 -0400

    2.4.0-alpha Additions to k3d_center_vectors()

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='magna',
-      version='2.3.4',
+      version='2.4.0-a',
       description='magnetic nanoparticle assembly utilities',
       url='https://github.com/sammysiegel/MAGNA-U',
       authors=['Sammy Siegel', 'Niels Vanderloo', 'Yumi Ijiri'],
